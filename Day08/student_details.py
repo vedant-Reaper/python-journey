12931c25404f2c584f0e7063605da13eb03c6022
Ved <raskarvedant9@gmail.com>
2026-07-17 20:43:52 +0530

    Add script to store and display student details using a dictionary
    Add student details display script
    
    - Store student info (Name, Age, Branch, College) in a dictionary
    - Loop through dictionary items and print formatted key-value pairs

---
+++ Day08/student_details.py
@@ -0,0 +1,10 @@
+student = {
+    "Name": "Vedant Raskar",
+    "Age": "19",
+    "RBT no": "RBT25ET098",
+    "Branch": "ENTC",
+}
+
+print("Student Details")
+for key, value in student.items():
+    print(f"{key} : {value}")
