4abab3158d0ba2ba869b1d5690160618342d6bed
Ved <raskarvedant9@gmail.com>
2026-07-19 11:40:07 +0530

    Learned how if else work in python

---
+++ Day09/number_comparison.py
@@ -0,0 +1,7 @@
+num1, num2 = int(input("Enter first number: ")), int(input("Enter second number: "))
+if num1 > num2:
+    print("First number is greater than second number.")
+elif num1 < num2:
+    print("Second number is greater than first number.")
+else:
+    print("Both numbers are equal.")
