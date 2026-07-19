2723cb35a0ee10c317bf3056db88a42f74f898c3
Ved <raskarvedant9@gmail.com>
2026-07-19 11:46:35 +0530

    Did the positive negeative numbers code :)

---
+++ Day09/postive_negative_number.py
@@ -0,0 +1,8 @@
+num1 = int(input ("Enter the your number: "))
+
+if num1 > 0:
+    print(f"The number {num1} is positive.")
+elif num1 < 0:
+    print(f"The number {num1} is negative.")
+else:
+    print(f"The number {num1} is zero 0.")
